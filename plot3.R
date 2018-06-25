cd3d22f03ac595df288dd9fb4ae1b7ada10a03d8
faaransaleem <39471806+faaransaleem@users.noreply.github.com>
2018-06-25 21:22:14 +0500

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,13 @@
+yes <- file('household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(yes),value=TRUE), sep = ';', na.strings = '?')
+names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$combdate <- as.POSIXct(paste(data$Date, data$Time))
+
+png(filename = 'plot3.png',  width = 480, height = 480, units='px')
+plot(data$combdate, data$Sub_metering_1, ylab = 'Energy sub metering', type = 'l',xlab= '')
+lines(data$combdate, data$Sub_metering_2, col = 'red')
+lines(data$combdate, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+dev.off()
+dev.off()
